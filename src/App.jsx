dff580a75a527114fdd7c224bf0e70a73d325b72
Agustin978 <loboagus10@gmail.com>
2023-06-05 02:49:57 -0300

    Creada una funcion para el filtrado de articulos y un arreglo que es el que se tendria que ir modificando con las diferentes busquedas, pero no lo hace del todo bien :(

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {Navbar, Container} from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Formulario from './Components/Formulario.jsx';
+import Formulario from './Components/Formulario';
 import ContenedorNoticias from './Components/ContenedorNoticias';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -11,7 +11,8 @@ function App() {
   
   //const [noticia, setNoticia] = useState({});
   const [noticias, setNoticias] = useState([]);
-  const [cantidadNoticias, setCantidadNoticias] = useState();
+  const [noticiasFiltradas, setNoticiasFiltradas] = useState([]);
+  //const [cantidadNoticias, setCantidadNoticias] = useState();
 
   useEffect(()=>{
     consultarApi();
@@ -23,16 +24,36 @@ function App() {
     {
       const retornoApi = await fetch('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=840f66d5a9f747d8879103256e63dca9');
       const datosApi = await retornoApi.json();
-      console.log(datosApi);
+      //console.log(datosApi);
       //console.log(datosApi.articles[0]);
-      setNoticias(datosApi.articles);
-      setCantidadNoticias(datosApi.totalResults);
+      const noticiasApi = datosApi.articles;
+      setNoticias(noticiasApi);
+      setNoticiasFiltradas(noticiasApi);
+      //setCantidadNoticias(datosApi.totalResults);
     }catch(error)
     {
       console.log('Como ez pozible ezte zuzezoo a ocurrido un error :´( :',error);
     }
   }
 
+  const filtraNoticias = (buscados) =>
+  {
+    if(buscados!='')
+    {
+      console.log(buscados);
+      let _noticiasFiltradas = noticias.filter((noticia) => noticia.title && noticia.title.includes(buscados));
+      console.log(_noticiasFiltradas);
+      setNoticiasFiltradas(_noticiasFiltradas);
+    }else
+    {
+      setNoticiasFiltradas(noticias);
+    }
+    /*
+    console.log(buscados);
+    console.log(noticias);
+    let _noticias = noticias.filter((noticia) => noticia.title.includes(buscados))
+    setNoticias(_noticias);*/
+  }
   //console.log(cantidadNoticias);
 
   return (
@@ -41,8 +62,8 @@ function App() {
           <h1 className='fw-bolder text-light m-auto p-3'>Noticias</h1>
       </Navbar>
       <Container className='mainPage mt-5'>
-        <Formulario />
-        <ContenedorNoticias noticias={noticias}/>
+        <Formulario filtraNoticias={filtraNoticias}/>
+        <ContenedorNoticias noticias={noticiasFiltradas}/>
       </Container>
       <footer className='bg-dark text-center text-secondary py-4'>
         <p className='display-6'>&copy; Todos los derechos reservados</p>
